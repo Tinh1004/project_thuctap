654d48eaabc01751cb3cc31b3059dcc4488a8027
Attiq <0703thuy@gmail.com>
2022-07-15 08:40:07 +0700

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,60 @@
-export default function LoginPage(props) {
+import React, {useState} from "react";
+
+ function LoginPage({Login, error}) {
+    const [details, setDetails] = useState({username:"", password:""});
+   
+    const submitHandler = e => {
+        e.preventDefault();
+        Login(details);
+    }
     return (
-        <section className="home">
-            <h1>Login Page</h1>   
-        </section>
-    )
+    <form onSubmit={submitHandler}>
+      <div className="inner-form">
+        <h1>Login</h1>
+        <button className="login-fb">
+          <i className="fa fa-facebook login-icon"></i>
+          <span className="login-social-text">Login with Facebook</span>
+        </button>
+        <button className="login-gg">
+          <i className="fa fa-google login-icon"></i>
+          <span className="login-social-text">Login with Google</span>
+        </button>
+        <div>
+          <div className="login-or">
+            <span>OR</span>
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">
+              Username
+            </label>
+            <input
+              type="text"
+              class="ls-input"
+              placeholder="Email or username"
+              onChange={e => setDetails({...details, name:e.target.value})} value={details.name}
+            />
+            <div className="form-group">
+              <label htmlFor="password">
+                Password
+              </label>
+              <input type="password" className="ls-input" placeholder="password" />
+            </div>
+            
+            <a href= " " className="forgot-password">Forgot Password?</a>
+ 
+            <button className="submit" type="submit">
+              Login
+            </button>
+            <p>
+              Don't have an account?
+              <a href="signup.html" className="link">
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 }
+export default LoginPage
