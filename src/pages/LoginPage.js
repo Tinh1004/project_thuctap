d900882942b126bc773668878406ffccb7fca7bb
Attiq <0703thuy@gmail.com>
2022-07-18 17:02:17 +0700

    Login facebook and google

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "../login.css";
 import { Link } from "react-router-dom";
+import Facebook from "../components/form_login/FacebookLogin";
+// import Facebook from "../components/form_login/FacebookLogin";
+import Google from "../components/form_login/GoogleLogin"
 function LoginPage({ Login, error }) {
   const [details, setDetails] = useState({ username: "", password: "" });
 
@@ -12,14 +15,16 @@ function LoginPage({ Login, error }) {
     <form onSubmit={submitHandler}>
       <div className="inner-form">
         <h1>Login</h1>
-        <button className="login-fb">
+        {/* <button className="login-fb">
           <i className="fa fa-facebook login-icon"></i>
           <span className="login-social-text">Login with Facebook</span>
-        </button>
-        <button className="login-gg">
+        </button> */}
+        <Facebook className="login-fb"/>
+        {/* <button className="login-gg">
           <i className="fa fa-google login-icon"></i>
           <span className="login-social-text">Login with Google</span>
-        </button>
+        </button> */}
+        <Google className="login-gg"/>
         <div>
           <div className="login-or">
             <span>OR</span>
