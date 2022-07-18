785618b215532a55160e244a5be302dc7e097dce
TonnyNguyen23 <nguyenxuantung473@gmail.com>
2022-07-18 17:08:44 +0700

    [tungnx] Trim Component Profile Page

---
+++ src/pages/profile_page/DetailPlaylistPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DetailPlaylist from "../../components/playlist/detail_playlist/DetailPlaylist";
+
+function DetailPlaylistPage() {
+  return <DetailPlaylist />;
+}
+
+export default DetailPlaylistPage;
