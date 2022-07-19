a4abb370e518fac1cf0ee6a2cf678cbaa22909f1
TonnyNguyen23 <nguyenxuantung473@gmail.com>
2022-07-19 10:40:16 +0700

    [tungnx] Add React-router-dom for Profile Page

--- src/App.js
+++ src/App.js
@@ -10,20 +10,13 @@ import LoginPage from "./pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import Datapage2 from "./components/category/page2/Datapage2";
-import SongComponent from "./components/menu/songs/SongComponent";
-import LibraryPlaylistPage from "./pages/profile_page/LibraryPlaylistPage";
-import DetailPlaylist from "./components/playlist/detail_playlist/DetailPlaylist";
 
 function App() {
   return (
     <Routes>
       <Route path="*" element={<NotFoundPage />} />
       <Route path="/" element={<HomePage />} />
-      <Route path="/ca-nhan">
-        <Route index element={<ProfilePage />} />
-        <Route path="library" element={<LibraryPlaylistPage />} />
-        <Route path="detail" element={<DetailPlaylist />} />
-      </Route>
+      <Route path="/ca-nhan/*" element={<ProfilePage />} />
       <Route path="/the-loai" element={<CategoryPage />} />
       <Route path="/page2" element={<Datapage2 />} />
       <Route path="/search">
--- src/pages/profile_page/ProfilePage.js
+++ src/pages/profile_page/ProfilePage.js
@@ -1,15 +1,23 @@
-import AppDefault from "../app_default/AppDefault";
-import PlayList from "../../components/playlist/index";
-import MenuProfile from "../../components/menu/index";
 import { Route, Routes } from "react-router-dom";
+
+import AppDefault from "../app_default/AppDefault";
+import LibraryPlaylistPage from "./LibraryPlaylistPage";
+import DetailPlaylistPage from "./DetailPlaylistPage";
+import ProfileContent from "./ProfileContent";
 import SongComponent from "../../components/menu/songs/SongComponent";
+import Podcast from "../../components/menu/podcast/Podcast";
+import Album from "../../components/menu/album/Album";
+import MvComponent from "../../components/menu/mv/MvComponent";
 
 export default function ProfilePage(props) {
   return (
     <AppDefault>
       <section className="home">
-        <PlayList />
-        <MenuProfile />
+        <Routes>
+          <Route path="/*" element={<ProfileContent />} />
+          <Route path="/library" element={<LibraryPlaylistPage />} />
+          <Route path="/detail" element={<DetailPlaylistPage />} />
+        </Routes>
       </section>
     </AppDefault>
   );
