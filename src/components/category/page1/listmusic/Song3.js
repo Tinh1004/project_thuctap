c6b4ebda867dd874de20398f5f46a3927a3fe1d8
IchNguyen <nguyenichnguyen06@gmail.com>
2022-07-25 11:36:20 +0700

    sua lai nd

--- src/components/category/page1/listmusic/Song3.js
+++ src/components/category/page1/listmusic/Song3.js
@@ -1,20 +1,26 @@
+/* eslint-disable no-undef */
 import React from 'react'
 import './song3.css'
+import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
-import { Link } from 'react-router-dom';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import { NavLink } from 'react-router-dom';
 export default function Song3(props) {
+    const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
   return (
     <>
-        <Link to="/the-loai/page3">
+        {/* <Link to="/the-loai/page3"> */}
             <div className='khoinhac2'>
                 <div className='nhac2'>
                     <div className='image1'>
                         <img className='hinhto2' src={props.img3}></img>
                         <div className='icon'>
-                            {/* <Checkbox {...label} sx={{color: blue[600]}} icon={<FavoriteBorder />} checkedIcon={<Favorite />} /> */}
-                            <i className="icon1 fa fa-heart-o" />
-                            <i className="icon2 fa fa-play-circle" aria-hidden="true" />
+                            <Checkbox sx={{color:'#fff'}} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+                            {/* <i className="icon1 fa fa-heart-o" /> */}
+                            <NavLink to="/the-loai/page3"><i className="icon2 fa fa-play-circle"/></NavLink>
                             <i className="icon3 fa fa-ellipsis-h" />
                         </div>
                     </div>
@@ -24,7 +30,7 @@ export default function Song3(props) {
                     </div>
                 </div>
             </div>
-        </Link>
+        {/* </Link> */}
     </>
   )
 }
