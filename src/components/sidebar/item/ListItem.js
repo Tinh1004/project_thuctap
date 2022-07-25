406e9cf0b71dfbf20a8769444ad10fa79dd1898d
PhamNgocTinh-ST <1004tinh@gmail.com>
2022-07-25 16:13:23 +0700

    [tinh] change sideBar

--- src/components/sidebar/item/ListItem.js
+++ src/components/sidebar/item/ListItem.js
@@ -1,9 +1,19 @@
 import Item from './Item'
 
+const item = {
+  title: "Tạo playlist",
+  icon: <i className='bx bx-plus icon'></i>,
+  url: "/dsdsa"
+}
 export default function ListItem({ data }) {
+
   return (
     <ul className="menu-links">
       {data.map((item, index) => <Item key={index} item={item} />)}
+      <li className="nav-link" style={{ cursor: 'pointer' }} onClick={() => { console.log("Long Occho") }}>
+        {item.icon}
+        <span className="text nav-text">{item.title}</span>
+      </li>
     </ul>
   )
 }
