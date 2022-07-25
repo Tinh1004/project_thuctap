10f108327898786f4e5b03c6236c4ce7f0e58f37
PhamNgocTinh-ST <1004tinh@gmail.com>
2022-07-25 15:55:38 +0700

    [tinh] Test

--- src/components/content/release/release_item/CardReleaseItem.jsx
+++ src/components/content/release/release_item/CardReleaseItem.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './styles.scss';
 
+import dataSlice from '../../../../redux/dataSlice/dataSlice';
+import {useDispatch} from 'react-redux';
+
 export default function ReleaseItem(props) {
+  const dispatch = useDispatch();
   return (
-    <div className='releaseItemContainer'>
+    <div className='releaseItemContainer' onClick={()=>{dispatch(dataSlice.actions.changeSong(1))}}>
         <img className='image' src={props.img} alt={props.alt} />
         <i className="far fa-play-circle" />
         <h6 className='name'>{props.name}
