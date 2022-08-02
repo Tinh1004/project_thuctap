cc6f7f1c4f3278f1a1f0ba8b90a6f7cf237d2e70
IchNguyen <nguyenichnguyen06@gmail.com>
2022-08-02 10:21:35 +0700

    responsive category page

--- src/components/category/page1/listmusic/Song3.js
+++ src/components/category/page1/listmusic/Song3.js
@@ -5,8 +5,13 @@ import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
 import { NavLink } from 'react-router-dom';
+
+// import { toast } from 'material-react-toastify';
+// import 'material-react-toastify/dist/ReactToastify.css';
+
 export default function Song3(props) {
     const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+    const notify = () => toast("Wow so easy !");
   return (
     <>
         {/* <Link to="/the-loai/page3"> */}
@@ -15,7 +20,7 @@ export default function Song3(props) {
                     <div className='image1'>
                         <img className='hinhto2' src={props.img3}></img>
                         <div className='icon'>
-                            <Checkbox sx={{color:'#fff'}} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+                            <Checkbox onClick={notify}  sx={{color:'#fff'}} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
                             {/* <i className="icon1 fa fa-heart-o" /> */}
                             <NavLink to={props.title}><i className="icon2 fa fa-play-circle"/></NavLink>
                             <i className="icon3 fa fa-ellipsis-h" />
