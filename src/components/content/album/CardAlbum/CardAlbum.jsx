2c297379ad35923fcd2e68370e08851a3cd5d09b
builethienphuc47 <thienphuc04072001@gmail.com>
2022-08-08 10:25:16 +0700

    backup

--- src/components/content/album/CardAlbum/CardAlbum.jsx
+++ src/components/content/album/CardAlbum/CardAlbum.jsx
@@ -1,41 +1,46 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./styles.scss";
-export default function CardAlbum(props) {
-  let navigate = useNavigate();
-  const navigateListMusic = () => {
-    switch (props.id) {
-      case 0:
-        navigate("/list-song-at-party");
-        break;
-      case 1:
-        navigate("/list-song-at-work");
-        break;
-      case 2:
-        navigate("/list-song-NCSRelease");
-        break;
-      case 3:
-        navigate("/list-song-party-summervibe");
-        break;
-      case 4:
-        navigate("/list-song-USUKTopHit");
-        break;
+export default function CardAlbum(item) {
+  // let navigate = useNavigate();
+  // const navigateListMusic = () => {
+  //   switch (props.id) {
+  //     case 0:
+  //       navigate("/list-song-at-party");
+  //       break;
+  //     case 1:
+  //       navigate("/list-song-at-work");
+  //       break;
+  //     case 2:
+  //       navigate("/list-song-NCSRelease");
+  //       break;
+  //     case 3:
+  //       navigate("/list-song-party-summervibe");
+  //       break;
+  //     case 4:
+  //       navigate("/list-song-USUKTopHit");
+  //       break;
 
-      default:
-        break;
-    }
-  };
+  //     default:
+  //       break;
+  //   }
+  // };
+
   return (
     <div className="cardAlbumContainer">
       <li className="cardAlbum">
-        <div className="img_play">
-          <img className="img" src={props.img} alt={props.alt} />
-          <i className="far fa-play-circle" onClick={navigateListMusic} />
-        </div>
-        <h5 className="info" onClick={navigateListMusic}>
-          {props.name}
-        </h5>
-        <div className="subtitle">{props.author}</div>
+        <Link to={`/detail/${item.id}`}>
+          <div className="img_play">
+              <img className="img" src={item.img} alt={"Loading...."} />
+              <i className="far fa-play-circle" />
+            {/* <i className="far fa-play-circle" onClick={navigateListMusic} /> */}
+          </div>
+        </Link>
+        <Link to={`/detail/${item.id}`} >
+          <h5 className="info">{item.name}</h5>
+        </Link>
+        <div className="subtitle">{item.author}</div>
       </li>
     </div>
   );
--- src/components/content/album/albums.jsx
+++ src/components/content/album/albums.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import "./styles.scss";
+import { useSelector } from "react-redux";
+import { playListsSelector } from "../../../redux/selectors";
 import CardAlbum from "./CardAlbum/CardAlbum";
-import { albums } from "./data/albums";
+import "./styles.scss";
+// import CardAlbum from "./CardAlbum/CardAlbum";
+// import { albums } from "./data/albums";
 export default function Albums() {
+  const albums = useSelector(playListsSelector)
   return (
     <div className="albumsContainer">
       <div className="albums">
         <h4>Albums Phổ Biến</h4>
       </div>
       <div className="cardAlbums">
-        {albums.map((item) => (
+        {albums.slice(0,5).map((item) => (
           <CardAlbum
             key={item.id}
             name={item.name}
             id = {item.id}
-            img={item.links.images[1].url}
-            author={item.author}
+            img={item.image}
+            author={item.discription}
           />
         ))}
       </div>
