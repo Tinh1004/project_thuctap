d235faab7c81dad7e98af4a5aa4bd783a8cce822
unknown <1004tinh@gmail.com>
2022-08-10 14:44:18 +0700

    [Tinh] add song in data.json

--- src/components/navbar/avatar/index.js
+++ src/components/navbar/avatar/index.js
@@ -9,6 +9,7 @@ import { userDataSelector } from "../../../redux/selectors";
 const IMAGE_AVATAR =
   "https://i.scdn.co/image/ab6761610000e5eb8ae7f2aaa9817a704a87ea36";
 
+
 export default function AvatarUser() {
   const [isClick, setIsClick] = useState(false);
   const userData = useSelector(userDataSelector);
